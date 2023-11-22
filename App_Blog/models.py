80f01b25a9a3e29106633e7b992ee6e2a5907a14
IamShihabPro <mshihab.pro@gmail.com>
2023-11-23 02:48:21 +0600

    blogs added

--- App_Blog/models.py
+++ App_Blog/models.py
@@ -13,6 +13,9 @@ class Blog(models.Model):
     publish_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-publish_date']
+
     def __str__(self) :
         return self.blog_title
     
@@ -23,10 +26,17 @@ class Comment(models.Model):
     comment = models.TextField()
     comment_date = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-comment_date']
+
     def __str__(self):
         return self.comment
+        
     
 
 class Likes(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='liked_blog')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='liked_user')
+
+    def __str__(self):
+        return self.user + " likes " + self.blog
--- App_Blog/urls.py
+++ App_Blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from App_Blog import views
 
 urlpatterns = [
-    path('', views.blog_list, name='blog_list'),
+    path('', views.BlogList.as_view(), name='blog_list'),
+    path('write/', views.CreateBlog.as_view(), name='create_blog'),
 ]
